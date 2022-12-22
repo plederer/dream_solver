5cc1ca45afd40b6f80405e8ed87092c67bd47893
Jan Ellmenreich <jan.ellmenreich@outlook.com>
2022-12-22 15:14:03 +0100

    Implemented Gradient Tests

--- testing/test_formulations.py
+++ testing/test_formulations.py
@@ -81,6 +81,7 @@ class TestFormulation(unittest.TestCase, ABC):
                 Q.Set(self.testing_gradient_U)
             elif mixed_method is MixedMethods.STRAIN_HEAT:
                 Q = test_gfu.components[2]
+                Q.Set(self.testing_mixed_heat_Q)
             else:
                 raise NotImplementedError()
 
@@ -102,6 +103,16 @@ class TestFormulation(unittest.TestCase, ABC):
             # Pressure Gradient
             error_msg = f"Wrong pressure gradient returned by {self.formulation} - {mixed_method}"
             is_value, expected_value = self.pressure_gradient_test(U, Q)
+            self.assertAlmostEqual(is_value, expected_value, msg=error_msg)
+
+            # Velocity Gradient
+            error_msg = f"Wrong velocity gradient returned by {self.formulation} - {mixed_method}"
+            is_value, expected_value = self.velocity_gradient_test(U, Q)
+            self.assertAlmostEqual(is_value, expected_value, msg=error_msg)
+
+            # Temperature Gradient
+            error_msg = f"Wrong temperature gradient returned by {self.formulation} - {mixed_method}"
+            is_value, expected_value = self.temperature_gradient_test(U, Q)
             self.assertAlmostEqual(is_value, expected_value, msg=error_msg)
 
         # Loop over all MixedMethods
@@ -154,6 +165,8 @@ class Test2DConservativeFormulation(TestFormulation):
         mesh = Mesh(OCCGeometry(Rectangle(2, 1).Face(), dim=2).GenerateMesh(maxh=0.5))
         cls.formulation = ConservativeFormulation2D(mesh, config)
 
+        gamma = config.heat_capacity_ratio.Get()
+
         cls.testing_U = CF((x*y,
                             x**3*y**2,
                             y**3*x**2,
@@ -162,6 +175,11 @@ class Test2DConservativeFormulation(TestFormulation):
                                      3*x**2*y**2, 2*x**3*y,
                                      2*y**3*x, 3*y**2*x**2,
                                      2*x*y**2, 2*y*x**2), dims=(4, 2))
+        cls.testing_mixed_heat_Q = CF((0,
+                                       0,
+                                       0,
+                                       gamma*(y - 2*x**3*y**2 - x*y**4),
+                                       gamma*(x - x**4*y - 2*x**2*y**3)))
         cls.testing_vector = CF((2, 1))
         cls.testing_matrix = CF((0.5, 1, 1.5, 2), dims=(2, 2))
 
@@ -269,175 +287,35 @@ class Test2DConservativeFormulation(TestFormulation):
         pressure_gradient = self.formulation.pressure_gradient(U, Q)
         is_value = Integrate(pressure_gradient * self.testing_vector, self.formulation.mesh)
 
-        expected_value = -(gamma-1)*160/12
+        expected_value = -(gamma-1)*136/12
 
         return is_value, expected_value
 
-    def velocity_gradient_test(self): ...
+    def velocity_gradient_test(self, U, Q):
 
-    def temperature_gradient_test(self): ...
-    # def test_density(self):
+        if Q is not None:
+            Q = Q.components
 
-    #     mesh = self.formulation.mesh
+        velocity_gradient = self.formulation.velocity_gradient(U, Q)
+        is_value = Integrate(InnerProduct(velocity_gradient, self.testing_matrix), self.formulation.mesh)
 
-    #     test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    #     exact_density_integral = 0.5
+        expected_value = 26/3
 
-    #     test_gfu = GridFunction(self.formulation.fes)
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
+        return is_value, expected_value
 
-    #     expected_density = self.formulation.density(U.components)
+    def temperature_gradient_test(self, U, Q):
 
-    #     error_msg = f"Wrong density returned by {self.formulation}"
-    #     self.assertAlmostEqual(Integrate(expected_density, mesh),
-    #                            exact_density_integral, msg=error_msg)
+        gamma = self.formulation.solver_configuration.heat_capacity_ratio.Get()
 
-    # def test_density_gradient(self):
+        if Q is not None:
+            Q = Q.components
 
-    #     solver_configuration = self.formulation.solver_configuration
-    #     mesh = self.formulation.mesh
+        temperature_gradient = self.formulation.temperature_gradient(U, Q)
+        is_value = Integrate(temperature_gradient * self.testing_vector, self.formulation.mesh)
 
-    #     test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    #     test_Q_gradient = CF((3*x**2 + y, x, 3*x**2, 0, y, x, 0, 2*y), dims=(4, 2))
-    #     some_vector = CF((2, 1))
-    #     exact_gradient_vector_integral = 3.5
+        expected_value = -gamma*20/3
 
-    #     # Test gradient of None and Strain-Heat mixed method
-    #     solver_configuration.mixed_method = None
-    #     test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-    #     Q = None
-
-    #     expected_density_gradient = self.formulation.density_gradient(U, Q)
-
-    #     error_msg = f"Wrong density gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    #     self.assertAlmostEqual(Integrate(expected_density_gradient * some_vector, mesh),
-    #                            exact_gradient_vector_integral, msg=error_msg)
-
-    #     # Test gradient of Gradient mixed method
-    #     solver_configuration.mixed_method = "gradient"
-    #     test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-    #     Q = test_gfu.components[2]
-    #     Q.Set(test_Q_gradient)
-
-    #     expected_density_gradient = self.formulation.density_gradient(U, Q.components)
-
-    #     error_msg = f"Wrong density gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    #     self.assertAlmostEqual(Integrate(expected_density_gradient * some_vector, mesh),
-    #                            exact_gradient_vector_integral, msg=error_msg)
-
-    # def test_momentum(self):
-
-    #     test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    #     some_vector = CF((2, 1))
-    #     exact_momentum_vector_integral = 0.75
-
-    #     test_gfu = GridFunction(self.formulation.fes)
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-
-    #     expected_momentum = self.formulation.momentum(U.components)
-
-    #     error_msg = f"Wrong momentum returned by {self.formulation}"
-    #     self.assertAlmostEqual(Integrate(expected_momentum * some_vector, self.formulation.mesh),
-    #                            exact_momentum_vector_integral, msg=error_msg)
-
-    # def test_momentum_gradient(self):
-
-    #     solver_configuration = self.formulation.solver_configuration
-
-    #     test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    #     test_Q_gradient = CF((3*x**2 + y, x, 3*x**2, 0, y, x, 0, 2*y), dims=(4, 2))
-    #     some_matrix = CF((0.5, 1, 1.5, 2), dims=(2, 2))
-    #     exact_gradient_matrix_integral = 2.25
-
-    #     # Test gradient of None and Strain-Heat mixed method
-    #     solver_configuration.mixed_method = None
-    #     test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-    #     Q = None
-
-    #     momentum_gradient = self.formulation.momentum_gradient(U, Q)
-
-    #     error_msg = f"Wrong momentum gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    #     self.assertAlmostEqual(Integrate(InnerProduct(momentum_gradient, some_matrix), self.formulation.mesh),
-    #                            exact_gradient_matrix_integral, msg=error_msg)
-
-    #     # Test gradient of Gradient mixed method
-    #     solver_configuration.mixed_method = "gradient"
-    #     test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-    #     Q = test_gfu.components[2]
-    #     Q.Set(test_Q_gradient)
-
-    #     momentum_gradient = self.formulation.momentum_gradient(U, Q.components)
-
-    #     error_msg = f"Wrong momentum gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    #     self.assertAlmostEqual(Integrate(InnerProduct(momentum_gradient, some_matrix), self.formulation.mesh),
-    #                            exact_gradient_matrix_integral, msg=error_msg)
-
-    # def test_energy(self):
-
-    #     test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    #     exact_energy_integral = 1/3
-
-    #     test_gfu = GridFunction(self.formulation.fes)
-    #     U = test_gfu.components[0]
-    #     U.Set(test_U)
-
-    #     expected_energy = self.formulation.energy(U.components)
-
-    #     error_msg = f"Wrong energy returned by {self.formulation}"
-    #     self.assertAlmostEqual(Integrate(expected_energy, self.formulation.mesh),
-    #                            exact_energy_integral, msg=error_msg)
-
-    # def test_energy_gradient(self):
-
-    # solver_configuration = self.formulation.solver_configuration
-
-    # test_U = CF((x**3 + y*x, x**3, y*x, y**2))
-    # test_Q_gradient = CF((3*x**2 + y, x, 3*x**2, 0, y, x, 0, 2*y), dims=(4, 2))
-    # some_vector = CF((2, 1))
-    # exact_gradient_vector_integral = 1
-
-    # # Test gradient of None and Strain-Heat mixed method
-    # solver_configuration.mixed_method = None
-    # test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    # U = test_gfu.components[0]
-    # U.Set(test_U)
-    # Q = None
-
-    # expected_density_gradient = self.formulation.energy_gradient(U, Q)
-
-    # error_msg = f"Wrong density gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    # self.assertAlmostEqual(Integrate(expected_density_gradient * some_vector, self.formulation.mesh),
-    #                        exact_gradient_vector_integral, msg=error_msg)
-
-    # # Test gradient of Gradient mixed method
-    # solver_configuration.mixed_method = "gradient"
-    # test_gfu = GridFunction(self.formulation.get_FESpace())
-
-    # U = test_gfu.components[0]
-    # U.Set(test_U)
-    # Q = test_gfu.components[2]
-    # Q.Set(test_Q_gradient)
-
-    # expected_density_gradient = self.formulation.energy_gradient(U, Q.components)
-
-    # error_msg = f"Wrong density gradient for {self.formulation} - {solver_configuration.mixed_method}"
-    # self.assertAlmostEqual(Integrate(expected_density_gradient * some_vector, self.formulation.mesh),
-    #                        exact_gradient_vector_integral, msg=error_msg)
+        return is_value, expected_value
 
 
 if __name__ == "__main__":
