f7fcbe08ac1aa1c98e9172a950ca845ae506f8e0
Jan Ellmenreich <jan.ellmenreich@outlook.com>
2023-01-05 09:10:02 +0100

    Refactored vortex example

--- examples/vortex.py
+++ examples/vortex.py
@@ -1,123 +1,81 @@
-import os
-import sys
-sys.path.insert(1, '../code')
-sys.path.insert(1, '../utils')
 from netgen.occ import OCCGeometry
-from netgen.geom2d import unit_square, SplineGeometry
 from ngsolve import *
-from HDGSolver import compressibleHDGsolver
-from geometries import *
-
-
+from dream import CompressibleHDGSolver, SolverConfiguration
+from dream.utils.geometries import MakeOCCRectangle, MakeOCCCircle, MakeOCCCirclePlane
 
 ngsglobals.msg_level = 0
 SetNumThreads(8)
 
-dirname = "pressure_nrbc"
-maxh = 5
-order = 5
+# geo = MakeOCCRectangle((-40, -40), (40, 40))
+geo = MakeOCCCircle((0, 0), 40)
+mesh = Mesh(geo.GenerateMesh(maxh=5))
 
-# Dimensionless equations with diameter D
-D = 1
-R = D/2
+cfg = SolverConfiguration()
 
-Pr = 0.72
-Re = 1
-Minf = 0.5
-gamma = 1.4
-mu = 1
-rhoinf = 1
+cfg.formulation = "conservative"
+cfg.simulation = "transient"
+cfg.dynamic_viscosity = "constant"
+cfg.mixed_method = "strain_heat"
 
-pinf = 1/Minf**2/gamma
-T_inf = 1 / (gamma - 1)/Minf**2
+cfg.Reynold_number = 1
+cfg.Mach_number = 0.3
+cfg.Prandtl_number = 0.72
+cfg.heat_capacity_ratio = 1.4
 
-Uinf = 1
-Vinf = 0
-Einf = pinf/(gamma-1)/rhoinf + 0.5 * (Uinf**2 + Vinf**2)
-inf_vals = (rhoinf, Uinf * rhoinf, Vinf * rhoinf, Einf * rhoinf)
+cfg.order = 2
+cfg.bonus_int_order_bnd = 10
+cfg.bonus_int_order_vol = 10
 
+cfg.time_scheme = "BDF2"
+cfg.time_step = 0.05
 
-Gamma = 5
-rv = 5
+cfg.linear_solver = "pardiso"
+cfg.damping_factor = 1
+cfg.max_iterations = 100
+cfg.convergence_criterion = 1e-12
+
+cfg.compile_flag = True
+cfg.static_condensation = True
+
+mesh.Curve(order=cfg.order)
+
+gamma = cfg.heat_capacity_ratio
+M = cfg.Mach_number
+
+rho_inf = 1
+T_inf = 1 / ((gamma - 1) * M**2)
+
+u_inf = CF((1, 0))
+p_inf = 1/(M**2 * gamma)
+
+Gamma = 1
+rv = 1
 psi = Gamma * exp(-(x**2 + y**2)/(2*rv**2))
 
-U0 = Uinf  # - psi.Diff(y)
-V0 = Vinf  # + psi.Diff(x)
-p0 = pinf + Gamma * exp(-(x**2 + y**2)/(2*rv**2))
-E0 = p0/(gamma-1)/rhoinf + 0.5 * (U0**2 + V0**2)
-
-zero_vals = (rhoinf, U0 * rhoinf, V0 * rhoinf, E0 * rhoinf)
-
-wp = MakeOCCRectangle((-40, -40), (40, 40), bottom="NRBC", right="NRBC", left="inflow", top="NRBC")
-geo = OCCGeometry(wp.Face(), dim=2)
-mesh = Mesh(geo.GenerateMesh(maxh=maxh))
-
-ff_data = {"Minf": Minf,
-           "Re": Re,
-           "Pr": Pr,
-           "mu": mu,
-           "gamma": gamma,
-           "dt": 0.1}
+u_0 = u_inf
+p_0 = p_inf + Gamma * exp(-(x**2 + y**2)/(2*rv**2))
 
 
-# bnd_data = {"inflow": ["inflow|outflow", inf_vals]}
-bnd_data = {"inflow": ["inflow", inf_vals],
-            "NRBC": ["NRBC", pinf]}
+solver = CompressibleHDGSolver(mesh, cfg)
+solver.boundary_conditions.set_farfield("left", rho_inf, u_inf, pressure=p_inf)
+solver.boundary_conditions.set_nonreflecting_outflow(
+    "right|bottom|top", pressure=p_inf, tangential_convective_fluxes=True, tangential_viscous_fluxes=True,
+    normal_viscous_fluxes=False)
+# solver.boundary_conditions.set_dirichlet("left|bottom|right|top", rho_inf, (u_inf, v_inf), pressure=p_inf)
+solver.initial_condition.set(rho_inf, u_inf, pressure=p_0)
 
-
-hdgsolver = compressibleHDGsolver(mesh,
-                                  order=order,
-                                  ff_data=ff_data,
-                                  bnd_data=bnd_data,
-                                  viscid=False,
-                                  stationary=True,
-                                  time_solver="BDF2",
-                                  force=inf_vals)
-
-
-uinit = CF(zero_vals)
-qinit = CoefficientFunction((0, 0, 0, 0, 0, 0, 0, 0), dims=(4, 2))
-
-hdgsolver.SetUp(condense=True)
-
-
-hdgsolver.InitializeDir(dirname)
-hdgsolver.SaveConfig()
-hdgsolver.SaveSolution()
-
-
-with TaskManager():
-    hdgsolver.SetInitial(uinit, qinit)
-    Redraw()
-    # input()
-    # hdgsolver.Solve(maxit=100, maxerr=1e-10, dampfactor=1, printing=True, max_dt=10)
-    hdgsolver.SaveState(0)
-
-
-hdgsolver.stationary = False
-hdgsolver.InitBLF()
-hdgsolver.DrawSolutions()
-
-Draw(hdgsolver.pressure - pinf, mesh, "p'")
-Draw(hdgsolver.velocity - CF((1, 0)), mesh, "u'")
-
-# hdgsolver.LoadState(0)
-
-tend = 70
 t = 0
-s = 0
-
-
-input()
-hdgsolver.gfu_old.vec.data = hdgsolver.gfu.vec
+tend = 40
 with TaskManager():
-    while t < tend:
-        t += hdgsolver.FU.dt.Get()
-        s += 1
-        print("step = {}, time = {}".format(s, t), end='\r')
-        hdgsolver.gfu_old_2.vec.data = hdgsolver.gfu_old.vec
-        hdgsolver.gfu_old.vec.data = hdgsolver.gfu.vec
-        hdgsolver.Solve(maxit=100, maxerr=1e-8, dampfactor=0.5, printing=True)
+    solver.setup()
+    solver.solve_initial()
+    solver.draw_solutions()
 
-        if s % 1 == 0:
-            hdgsolver.SaveState(s)
+    formulation = solver.formulation
+
+    Draw(formulation.pressure(solver.gfu.components[0]) - p_inf, mesh, "p'")
+    Draw(formulation.velocity(solver.gfu.components[0]) - u_inf, mesh, "u'")
+
+    while t < tend:
+        t += solver.solver_configuration.time_step.Get()
+        solver.solve_timestep(True)
