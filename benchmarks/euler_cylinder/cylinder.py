51f956d118e24922fa576a9cac91f47539dc5884
Jan Ellmenreich <jan.ellmenreich@outlook.com>
2023-01-17 17:55:13 +0100

    Finish Euler Cylinder Benchmark

--- benchmarks/euler_cylinder/cylinder.py
+++ benchmarks/euler_cylinder/cylinder.py
@@ -1,68 +1,78 @@
-from ngsolve import *
+"""
+Inviscid non-lifiting flow over a circular cylinder
 
-from dream import CompressibleHDGSolver, SolverConfiguration
+We benchmark the compressible Solver in the special case of an
+inviscid non-lifting flow around a circular cylinder.
+From literature it is well known, that the analytic solution is composed
+by the superposition of two potential flows, namely the uniform flow
+and doublet flow.
+
+As validation, we compare the numerical pressure coefficient along the
+cylinder surface against the analytical solution. The exact pressure coefficient
+is given by c_p = 1 - 4 * sin(phi)**2.
+
+Literature:
+[1] - J.D. Anderson,
+      Fundamentals of Aerodynamics, 6th edition
+      New York, NY: McGraw-Hill Education, 2017.
+"""
+from ngsolve import *
+from dream import *
 from dream.utils.meshes import Get_Omesh
 
 ngsglobals.msg_level = 0
 SetNumThreads(8)
 
-
 cfg = SolverConfiguration()
-# Formulation settings
 cfg.formulation = 'conservative'
 cfg.mixed_method = None
 cfg.dynamic_viscosity = None
 cfg.riemann_solver = "hllem"
-
-# Flow settings
 cfg.Mach_number = 0.1
 cfg.heat_capacity_ratio = 1.4
-
-# Solver settings
 cfg.order = 3
 cfg.damping_factor = 1.0
 cfg.time_scheme = 'BDF2'
-cfg.time_step = 0.05
-cfg.max_iterations = 100
+cfg.time_step = 0.001
+cfg.max_iterations = 300
 cfg.convergence_criterion = 1e-12
 cfg.linear_solver = 'pardiso'
-cfg.simulation = 'transient'
+cfg.simulation = 'stationary'
 cfg.compile_flag = True
 
-# Dimensionless equations with diameter D
-D = 1
-R = D/2
-R_farfield = D * 30
+R = 1
+R_farfield = R * 30
+r = sqrt(x**2 + y**2)
 
 rho_inf = 1
 u_inf = CF((1, 0))
 p_inf = 1/(cfg.Mach_number**2 * cfg.heat_capacity_ratio)
+u_0 = (r - R)/(R_farfield - R) * u_inf
 
 mesh = Mesh(Get_Omesh(R, R_farfield, 32, 16, geom=1.9))
 mesh.Curve(cfg.order)
 
+tree = ResultsDirectoryTree()
+points = PointSensor.vertices_from_boundaries('cyl', mesh)
+sensor = PointSensor(points, "pressure_coefficient", tree)
+sensor.sample_pressure_coefficient(p_inf, reference_velocity=1, reference_density=rho_inf, name="c_p")
+
 solver = CompressibleHDGSolver(mesh, cfg)
 solver.boundary_conditions.set_farfield('inflow', rho_inf, u_inf, pressure=p_inf)
-solver.boundary_conditions.set_nonreflecting_outflow('outflow', p_inf)
+solver.boundary_conditions.set_outflow('outflow', p_inf)
 solver.boundary_conditions.set_inviscid_wall('cyl')
-solver.initial_condition.set(rho_inf, u_inf, pressure=p_inf)
+solver.initial_condition.set(rho_inf, u_0, pressure=p_inf)
 
-saver = solver.get_saver("test2")
+sensor.assign_solver(solver)
+saver = solver.get_saver(tree)
 
-t = 0
-tend = 40
 with TaskManager():
-    saver.save_configuration()
-    saver.save_mesh()
     solver.setup()
     solver.solve_initial()
     solver.draw_solutions()
+    solver.solve_timestep(True, stat_step=50)
 
-    formulation = solver.formulation
-
-    Draw(formulation.pressure(solver.gfu.components[0]) - p_inf, mesh, "p'")
-    Draw(formulation.velocity(solver.gfu.components[0]) - u_inf, mesh, "u'")
-
-    while t < tend:
-        t += solver.solver_configuration.time_step.Get()
-        solver.solve_timestep(True)
+sensor.take_single_sample()
+saver.save_configuration(comment=__doc__)
+saver.save_mesh()
+saver.save_sensors_data([sensor])
---
+++ benchmarks/euler_cylinder/plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dream import SolverLoader
+
+# Load Dataframe
+loader = SolverLoader()
+df = loader.load_sensor_data('pressure_coefficient')
+
+# Extract Coords and Pressure Coefficient
+coords = np.array([point for point in df.columns.levels[1]])
+x, y = coords[:, 0], coords[:, 1]
+c_p = df.iloc[-1].to_numpy()
+
+# Calculate Angle and Sort
+phi = np.angle(x + 1j*y)
+c_p = c_p[np.argsort(phi)]
+phi.sort()
+
+# Exact solution
+phi_exact = np.linspace(-np.pi, np.pi, 100)
+c_p_exact = 1 - 4 * np.sin(phi_exact)**2
+
+# Draw
+fig = plt.figure(figsize=(5, 5), dpi=100)
+ax = fig.subplots(1, 1)
+ax.plot(phi_exact, c_p_exact, color='k')
+ax.scatter(phi, c_p, color='red')
+ax.set_xlabel(r"$\phi$")
+ax.set_ylabel(r"$c_p$")
+ax.set_xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
+ax.set_xticklabels([r'$-\pi$', r'$-\frac{\pi}{2}$', r'$0$', r'$\frac{\pi}{2}$', r'$\pi$'])
+fig.savefig("pressure_coefficient.png")
