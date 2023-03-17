baa7b96db02b620282dfea9723686268339b7ff8
Jan Ellmenreich <jan.ellmenreich@outlook.com>
2023-03-17 16:07:17 +0100

    Updated Examples

--- examples/visc_cylinder.py
+++ examples/visc_cylinder.py
@@ -11,7 +11,7 @@ SetNumThreads(8)
 geo = SplineGeometry()
 R = 1
 R_farfield = R * 30
-mesh = Mesh(Get_Omesh(R, R_farfield, 36, 16, geom = 1.5))
+mesh = Mesh(Get_Omesh(R, R_farfield, 36, 16, geom=1.5))
 Draw(mesh)
 ##################################################################
 cfg = SolverConfiguration()
@@ -23,13 +23,14 @@ cfg.mixed_method = "strain_heat"
 cfg.riemann_solver = "lax_friedrich"
 
 cfg.Reynolds_number = 10
-cfg.Prandtl_number = 1 #0.72
+cfg.Prandtl_number = 1  # 0.72
 cfg.Mach_number = 0.3
 cfg.heat_capacity_ratio = 1.4
 
 cfg.order = 2
 cfg.time_scheme = "IE"
 cfg.time_step = 0.1
+cfg.time_period = (0, 200)
 cfg.linear_solver = "pardiso"
 cfg.damping_factor = 1
 cfg.max_iterations = 100
@@ -42,7 +43,7 @@ mesh.Curve(cfg.order)
 gamma = cfg.heat_capacity_ratio
 M_inf = cfg.Mach_number
 
-u_inf = CF((1,0))
+u_inf = CF((1, 0))
 p_inf = 1
 rho_inf = p_inf * gamma / (u_inf[0]/M_inf)**2
 
@@ -50,19 +51,16 @@ rho_inf = p_inf * gamma / (u_inf[0]/M_inf)**2
 solver = CompressibleHDGSolver(mesh, cfg)
 solver.boundary_conditions.set_farfield("inflow", rho_inf, u_inf, pressure=p_inf)
 solver.boundary_conditions.set_outflow("outflow", pressure=p_inf)
-solver.boundary_conditions.set_adiabatic_wall("cyl")
+solver.boundary_conditions.set_adiabatic_wall("cylinder")
 solver.domain_conditions.set_initial(rho_inf, u_inf, pressure=p_inf)
 
 with TaskManager():
     solver.setup()
     solver.solve_initial()
-    solver.draw_solutions()
-    
-    formulation = solver.formulation
 
-    Draw(formulation.pressure(solver.gfu.components[0]) - p_inf, mesh, "p'")
-    Draw(formulation.velocity(solver.gfu.components[0]) - u_inf, mesh, "u'")
+    solver.drawer.draw_acoustic_pressure(p_inf)
+    solver.drawer.draw_particle_velocity(u_inf)
+    solver.drawer.draw_mach_number()
+    solver.drawer.draw_deviatoric_stress_tensor()
 
-    solver._solve_timestep(True, max_dt=1)
-    Redraw()
-    
+    solver.solve_transient()
