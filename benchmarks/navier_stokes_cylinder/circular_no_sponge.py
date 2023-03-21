ef1e54a58302c3ee4325fc5135d92e0c8a8fe795
Philip <philip@u24.at>
2023-03-21 17:45:00 +0100

    velocity pertubation

--- benchmarks/navier_stokes_cylinder/circular_no_sponge.py
+++ benchmarks/navier_stokes_cylinder/circular_no_sponge.py
@@ -3,7 +3,7 @@ from dream.utils.meshes import circular_cylinder_mesh
 from ngsolve import *
 
 ngsglobals.msg_level = 2
-SetNumThreads(8)
+SetNumThreads(4)
 
 
 cfg = SolverConfiguration()
@@ -15,7 +15,7 @@ cfg.Mach_number = 0.3
 cfg.Reynolds_number = 150
 cfg.Prandtl_number = 0.75
 cfg.heat_capacity_ratio = 1.4
-cfg.order = 3
+cfg.order = 2
 cfg.compile_flag = True
 cfg.damping_factor = 1
 cfg.time_scheme = 'BDF2'
@@ -32,13 +32,13 @@ mesh = circular_cylinder_mesh(radius=0.5,
                               sponge_layer=False,
                               boundary_layer_levels=1,
                               boundary_layer_thickness=0.08,
-                              transition_layer_levels=4,
+                              transition_layer_levels=2,
                               transition_layer_growth=1.2,
                               transition_radial_factor=8,
                               farfield_radial_factor=100,
                               sponge_radial_factor=200,
                               wake_maxh=1,
-                              farfield_maxh=8)
+                              farfield_maxh=5)
 mesh = Mesh(mesh)
 mesh.Curve(cfg.order)
 
@@ -55,6 +55,7 @@ loader = solver.get_loader()
 saver = solver.get_saver()
 saver.save_mesh(name='mesh')
 
+
 # Solve Stationary
 cfg.time_step = 0.01
 cfg.time_step_max = 10
@@ -67,9 +68,54 @@ with TaskManager():
 saver.save_configuration(name='steady_configuration')
 saver.save_state(name='intermediate_0', save_time_scheme_components=True)
 
+solver.setup()
+solver.drawer.draw()
+
+loader.load_state('intermediate_0', False)
+
+Gamma = 1
+Rv = 1
+Mx = -3
+My = 0.1
+r = sqrt((x-Mx)**2 + (y-My)**2)
+p_0 = Gamma * exp(-r**2/(Rv**2))
+psi = Gamma * exp(-r**2/(Rv**2))
+u_0 = CF((psi.Diff(y), -psi.Diff(x)))
+rho_0 = (p_0/p_inf)**(1/cfg.heat_capacity_ratio) * rho_inf
+rhoE_0 = p_0/(cfg.heat_capacity_ratio - 1) + rho_0 * 0.5 * (u_0[0]**2 + u_0[1]**2)
+bump = CF((rho_0,rho_0 * u_0[0],rho_0 * u_0[1],rhoE_0))
+
+gfu_pert = GridFunction(solver.formulation.fes)
+
+
+fes = solver.formulation.fes
+G, S = solver.formulation.TnT.MIXED
+U, V = solver.formulation.TnT.PRIMAL
+Uhat, Vhat = solver.formulation.TnT.PRIMAL_FACET
+
+m = BilinearForm(fes)
+m += G * S * dx()
+m += U * V * dx()
+m += Uhat * Vhat * dx(element_boundary = True)
+m.Assemble()
+
+f = LinearForm(fes)
+f +=  bump * V * dx()
+f += bump * Vhat * dx(element_boundary = True)
+f.Assemble()
+
+minv = m.mat.Inverse(fes.FreeDofs())
+gfu_pert.vec.data = minv * f.vec
+
+solver.formulation.gfu.vec.data += gfu_pert.vec
+solver.formulation.time_scheme.update_initial_solution(solver.formulation.gfu, *solver.formulation._gfu_old)
+
+Redraw()
+
+
 # Solve coarse transient
-cfg.time_step = 1
-cfg.time_period = (0, 500)
+cfg.time_step = 0.1
+cfg.time_period = (0, 100)
 cfg.convergence_criterion = 1e-8
 
 with TaskManager():
@@ -79,19 +125,12 @@ saver.save_configuration(name="transient_configuration_coarse")
 saver.save_state(f"intermediate_{cfg.time_period.end}", save_time_scheme_components=True)
 
 # Solve fine transient
-cfg.time_step = 0.001
-cfg.time_period = (500, 800)
+cfg.time_step = 0.01
+cfg.time_period = (100, 300)
 cfg.convergence_criterion = 1e-12
+cfg.save_state = True
 
 with TaskManager():
     solver.solve_transient()
 saver.save_configuration(name="transient_configuration_fine")
-saver.save_state(f"intermediate_{cfg.time_period.end}", save_time_scheme_components=True)
-
-# Solve fine transient - save states
-cfg.time_period = (800, 1000)
-cfg.save_state = True
-
-with TaskManager():
-    solver.solve_transient("transient", save_state_at_step=5)
 saver.save_state(f"intermediate_{cfg.time_period.end}", save_time_scheme_components=True)
