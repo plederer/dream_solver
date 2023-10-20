f7553612e1998672ad91014f06be2062de23c532
Philip <philip@u24.at>
2023-10-20 12:20:41 +0200

    conf file

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 sys.path.append(os.path.abspath('../dream'))
+sys.path.append(os.path.abspath('./dream'))
 sys.path.append(os.path.abspath('.'))
 # print(sys.path)
 
