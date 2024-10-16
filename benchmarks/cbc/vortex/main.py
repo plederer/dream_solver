f438c02ca6a9afcd61fae92fea2a36c8a6cf6a98
Jan Ellmenreich <jan.ellmenreich@outlook.com>
2024-10-16 10:53:50 +0200

    MOD Vortex benchmark

--- benchmarks/cbc/vortex/main.py
+++ benchmarks/cbc/vortex/main.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Vortex benchmark')
 parser.add_argument('Base Mach', metavar='M', type=float, help='Mach number of base flow')
 parser.add_argument('Vortex Mach', metavar='Mt', type=float, help='Mach number of vortex')
 args = vars(parser.parse_args())
+# args = {'Base Mach': 0.03, 'Vortex Mach': 0.01}
 
 ngsglobals.msg_level = 0
 SetNumThreads(64)
@@ -22,7 +23,7 @@ LOGGER.log_to_terminal = False
 saver = Saver(tree)
 
 name = ""
-maxh = 0.1
+maxh = 0.15
 L = 4
 H = 2
 Mt = args['Vortex Mach']
@@ -44,7 +45,7 @@ cfg.time.simulation = "transient"
 cfg.time.scheme = "BDF2"
 cfg.time.step = 2e-3
 save_step = 5
-cfg.time.interval = (0, 10)
+cfg.time.interval = (0, 5)
 cfg.save_state = True
 
 cfg.linear_solver = "pardiso"
@@ -59,6 +60,7 @@ face = WorkPlane().RectangleC(L, H).Face()
 
 for bc, edge in zip(['bottom', 'right', 'top', 'left'], face.edges):
     edge.name = bc
+face.edges[1].maxh = 0.1
 
 mesh = Mesh(OCCGeometry(face, dim=2).GenerateMesh(maxh=maxh))
 gamma = cfg.heat_capacity_ratio
@@ -72,7 +74,7 @@ T_inf = farfield.temperature
 c = INF.speed_of_sound(cfg)
 
 
-# Vortex Pirozzoli
+# Vortex
 
 r = sqrt(x**2 + y**2)
 
@@ -86,7 +88,7 @@ elif cfg.scaling is cfg.scaling.AEROACOUSTIC:
 psi = vt * R * exp((1 - (r/R)**2)/2)
 u_0 = u_inf + CF((psi.Diff(y), -psi.Diff(x)))
 p_0 = p_inf * (1 - (gamma - 1)/2 * Mt**2 * exp(1 - (r/R)**2))**(gamma/(gamma - 1))
-rho_0 = rho_inf * (1 - (gamma - 1)/2 * Mt**2 *  exp(1 - (r/R)**2))**(1/(gamma - 1))
+rho_0 = rho_inf * (1 - (gamma - 1)/2 * Mt**2 * exp(1 - (r/R)**2))**(1/(gamma - 1))
 initial = State(u_0, rho_0, p_0)
 p_00 = p_inf * (1 - (gamma - 1)/2 * Mt**2 * exp(1))**(gamma/(gamma - 1))
 
@@ -139,43 +141,112 @@ def test(name: str = ""):
 
 
 @test(name)
-def farfield_boundary():
-    solver = CompressibleHDGSolver(mesh, cfg, tree)
-    solver.boundary_conditions.set(bcs.FarField(farfield, Qform=True), "left|right|top|bottom")
-    return solver
-
-@test(name)
-def grcbc_farfield_outflow():
+def standard_farfield():
     solver = CompressibleHDGSolver(mesh, cfg, tree)
     solver.boundary_conditions.set(bcs.CBC(farfield, sigma=State(velocity=0.01, pressure=0.01)), 'left|top|bottom')
-    solver.boundary_conditions.set(bcs.CBC(farfield, relaxation="farfield", convective_tangential_flux=True, sigma=State(velocity=0.01, pressure=0.01)), 'right')
+    solver.boundary_conditions.set(bcs.FarField(farfield, Qform=True), "right")
     return solver
 
+
 @test(name)
-def grcbc_pressure_outflow():
+def grcbc_farfield(C: float = 0.01):
     solver = CompressibleHDGSolver(mesh, cfg, tree)
     solver.boundary_conditions.set(bcs.CBC(farfield, sigma=State(velocity=0.01, pressure=0.01)), 'left|top|bottom')
-    solver.boundary_conditions.set(bcs.CBC(farfield, relaxation="outflow", convective_tangential_flux=True, sigma=State(velocity=0.01, pressure=0.01)), 'right')
+    solver.boundary_conditions.set(
+        bcs.CBC(
+            farfield, relaxation="farfield", convective_tangential_flux=True, sigma=State(
+                velocity=C, pressure=C)),
+        'right')
+
+    tree.state_directory_name += f"_C{C}"
+    cfg.info['CFL'] = C
+
     return solver
 
-@test(name)
-def nscbc_pressure_outflow():
-    solver = CompressibleHDGSolver(mesh, cfg, tree)
-    solver.boundary_conditions.set(bcs.CBC(farfield, "nscbc"), 'left|top|bottom')
-    solver.boundary_conditions.set(bcs.CBC(farfield, "nscbc", relaxation="outflow", convective_tangential_flux=True, viscous_fluxes=True), 'right')
-    return solver
 
 @test(name)
-def outflow_boundary():
+def grcbc_outflow(C: float = 0.01):
     solver = CompressibleHDGSolver(mesh, cfg, tree)
-    solver.boundary_conditions.set(bcs.FarField(farfield, Qform=True), 'left|top|bottom')
+    solver.boundary_conditions.set(bcs.CBC(farfield, sigma=State(velocity=0.01, pressure=0.01)), 'left|top|bottom')
+    solver.boundary_conditions.set(bcs.CBC(
+        farfield, relaxation="outflow", convective_tangential_flux=True,
+        sigma=State(velocity=C, pressure=C)),
+        'right')
+
+    tree.state_directory_name += f"_C{C}"
+    cfg.info['CFL'] = C
+
+    return solver
+
+
+@test(name)
+def nscbc_outflow(Sigma: float = 0.01):
+    solver = CompressibleHDGSolver(mesh, cfg, tree)
+    solver.boundary_conditions.set(bcs.CBC(farfield, sigma=State(velocity=0.01, pressure=0.01)), 'left|top|bottom')
+    solver.boundary_conditions.set(
+        bcs.CBC(
+            farfield, "nscbc", relaxation="outflow", convective_tangential_flux=True,
+            sigma=State(velocity=Sigma, pressure=Sigma)),
+        'right')
+
+    tree.state_directory_name += f"_Sigma{Sigma}"
+    cfg.info['Sigma'] = Sigma
+
+    return solver
+
+
+@test(name)
+def standard_outflow():
+    solver = CompressibleHDGSolver(mesh, cfg, tree)
+    solver.boundary_conditions.set(bcs.CBC(farfield, sigma=State(velocity=0.01, pressure=0.01)), 'left|top|bottom')
     solver.boundary_conditions.set(bcs.Outflow(farfield.pressure), 'right')
     return solver
 
 
+def exact_solution():
+
+    tree.directory_name = f"Ma{cfg.Mach_number.Get()}/Mat{Mt}/dt{cfg.time.step.Get()}"
+    tree.state_directory_name = "exact"
+
+    save_step = 5
+    time = cfg.time.to_array()[::save_step]
+
+    fes = L2(mesh, order=cfg.order)**4
+    gfu = GridFunction(fes)
+
+    saver = Saver(tree)
+
+    with TaskManager():
+
+        for t in time:
+
+            r = sqrt((x - t)**2 + y**2)
+
+            vt = Mt/cfg.Mach_number
+            psi = vt * R * exp((1 - (r/R)**2)/2)
+            p = p_inf * (1 - (gamma - 1)/2 * Mt**2 * exp(1 - (r/R)**2))**(gamma/(gamma - 1))
+            u = u_inf + CF((psi.Diff(y), -psi.Diff(x)))
+            rho = rho_inf * (1 - (gamma - 1)/2 * Mt**2 * exp(1 - (r/R)**2))**(1/(gamma - 1))
+
+            gfu.Set((rho, rho*u, p/(gamma - 1) + 0.5*rho*InnerProduct(u, u)))
+
+            Draw(p, mesh, "p")
+
+            saver.save_state(gfu, f'transient_{t}')
+
+        saver.save_configuration(cfg, name=f"{tree.state_directory_name}/cfg")
+
+
 if __name__ == '__main__':
-    grcbc_pressure_outflow()
-    grcbc_farfield_outflow()
-    nscbc_pressure_outflow()
-    farfield_boundary()
-    outflow_boundary()
+
+    exact_solution()
+
+    standard_farfield()
+    standard_outflow()
+
+    for C in [1e-1, 1e-2, 0]:
+        grcbc_outflow(C)
+        grcbc_farfield(C)
+
+    for Sigma in [1, 0.28, 1e-2]:
+        nscbc_outflow(Sigma)
