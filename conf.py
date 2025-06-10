d8228e3648e46f857e1bbdad4898673c06d1a9c7
Philip <philip.lederer@uni-hamburg.de>
2025-06-10 17:56:38 +0200

    [UPD] examples for scalar convection equation

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ extensions = ["sphinxcontrib.bibtex",
               ]
 
 templates_path = ['_templates']
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', "**.ipynb_checkpoints", "examples"]
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', "**.ipynb_checkpoints"] #, "examples"]
 
 tikz_proc_suite = 'pdf2svg'
 
